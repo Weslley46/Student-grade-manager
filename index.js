83ea1d7c36ed9466bd9704a563a0cc7fb5491b1f
Weslley46 <vortezx777@gmail.com>
2025-08-20 17:15:33 -0300

    GET and POST inserted

---
+++ index.js
@@ -0,0 +1,41 @@
+import http from 'http'
+import {v4} from 'uuid'
+
+
+const port = 3000
+const grades  = [
+    {
+    studentName : 'John Doe',
+    subject : 'Math',
+    grade : 8,
+    },
+];
+
+const server = http.createServer((request, response) => {
+   const {method, url} = request
+    let body = '';
+    request.on('data', chunk => {
+      body += chunk.toString ();
+    })
+
+    request.on('end', () => {
+           if ( url === '/grades' && method === 'GET') {
+       response.writeHead(200, {'Content-type ': 'application/json'}) 
+         response.end(JSON.stringify(grades))
+    } else if (url === '/grades' && method === 'POST') {
+      const { studentName, subject, grade } = JSON.parse(body);
+      const newGrade = {id : v4 (), studentName, subject, grade };
+      grades.push(newGrade);
+      response.writeHead(201, {'Content-type': 'application/json'});
+      response.end(JSON.stringify(newGrade));
+    } else {
+        response.writeHead(404, {'Content-type': 'application/json'});
+        response.end(JSON.stringify({ message : ' Route not Found' }));
+    } 
+    });
+  });
+
+  server.listen(port, () => { `Server is running on port ${port}`
+    console.log
+  });
+  
